8608f6a1abdaa63d09309b0fb7a1f9efaed54f32
jiaming743 <743192023@qq.com>
2019-04-30 17:07:48 +0800

    Bug fixes: Abnormal calculation due to spaces

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ export function getOpacity (color) {
 
   if (!isRgba) return 1
 
-  return Number(color.replace(/^(rgba\()|(\d+,){3}|(\))/g, ''))
+  return Number(color.replace(/^(rgba\()|(\s*\d+\s*,){3}|(\))/g, ''))
 }
 
 /**
